4c48a1f9c1d03c018f7e71a6cafcdbe3aaf278be
karthik96siddhu <karthik96siddhu@gmail.com>
2020-10-09 13:09:12 +0530

    removing unwanted files

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app-main",
+    templateUrl: "./app.component.html",
+    styles: ["./app.component.css"]
+})
+export class AppComponent {}
---
+++ core-angular/src/polyfills.ts
@@ -0,0 +1,2 @@
+// angular create this file when create project through cli. Through this file angular module and comp with decorators is created. 
+import "zone.js/dist/zone";
